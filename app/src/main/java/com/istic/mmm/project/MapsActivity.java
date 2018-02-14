cc9f6ed36620d6fc46fbcdb93604cdfbaa10d1a2
Pierre-Louis <pl.ollivier29@gmail.com>
2018-02-14 16:07:21 +0100

    Add Map

--- app/src/main/java/com/istic/mmm/project/MapsActivity.java
+++ app/src/main/java/com/istic/mmm/project/MapsActivity.java
@@ -1,13 +1,28 @@
 package com.istic.mmm.project;
 
+import android.*;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -37,10 +52,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMinZoomPreference(12.1f);
+        mMap.setMaxZoomPreference(16.0f);
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng rennes = new LatLng(48.1173421, -1.7075198);
+        LatLng carrefour1 = new LatLng(48.10990899999999, -1.6565067999999883);
+        LatLng carrefour3 = new LatLng(48.0830207, -1.6810617999999522);
+        LatLng carrefour4 = new LatLng(48.1147851, -1.6768945999999687);
+        LatLng carrefour5 = new LatLng(48.1103767, -1.6705676000000267);
+        mMap.addMarker(new MarkerOptions().position(carrefour1).title("Carrefour 1"));
+        mMap.addMarker(new MarkerOptions().position(carrefour3).title("Carrefour 3"));
+        mMap.addMarker(new MarkerOptions().position(carrefour4).title("Carrefour 4"));
+        mMap.addMarker(new MarkerOptions().position(carrefour5).title("Carrefour 5"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(rennes));
     }
 }
